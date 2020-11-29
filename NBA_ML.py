4d14b0c6ecd456ef66aa46dfb7331f513dc36ad8
bwilder95 <brentonwilder1995@gmail.com>
2020-11-28 23:39:42 -0800

    hello world!

---
+++ NBA_ML.py
@@ -0,0 +1,12 @@
+# Final Project - Using ML and NBA games to predict winners
+# Brenton Wilder
+import sys
+
+
+def main():
+
+    print("Hello world!")
+
+
+if __name__ == "__main__":
+    sys.exit(main())
