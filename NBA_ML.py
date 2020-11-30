48a42faa3e81b2008cf3262b4e630a33fbdbb1c4
bwilder95 <brentonwilder1995@gmail.com>
2020-11-29 17:58:47 -0800

    win percent

--- NBA_ML.py
+++ NBA_ML.py
@@ -18,15 +18,19 @@ def main():
     nan_value = float("NaN")
     df.replace("", nan_value, inplace=True)
     df = df.dropna()
-    df = df.sort_values("GAME_DATE_EST").set_index(["GAME_DATE_EST"])
+    df.drop_duplicates()
+    df.sort_values("GAME_DATE_EST")
+    df.set_index("GAME_DATE_EST")
 
-    df_tms = pd.read_csv("./data/teams.csv", na_filter=False)
-    df_tms.replace("", nan_value, inplace=True)
-    df_tms = df_tms.dropna()
+    df_tm = pd.read_csv("./data/teams.csv", na_filter=False)
+    df_tm.replace("", nan_value, inplace=True)
+    df_tm = df_tm.dropna()
 
     df_rnk = pd.read_csv("./data/ranking.csv", na_filter=False)
     df_rnk.replace("", nan_value, inplace=True)
     df_rnk = df_rnk.dropna()
+    df_rnk.sort_values("STANDINGSDATE")
+    df_rnk.set_index("STANDINGSDATE")
 
     # Test model using only existing features in games
     # Don't use points home or points (win team scores more)
@@ -55,18 +59,58 @@ def main():
     # Begin Feature Engineering
     # Feature 1: Home Team arena capacity
     # Feature 2: Year Home Team was founded
-    # Feature 3: Conference, West=0 and East=1
-    df_tms = df_tms.rename(columns={"TEAM_ID": "HOME_TEAM_ID"})
+    # Feature 3: Conference of Home Team, West=0 and East=1
+    homID = "HOME_TEAM_ID"
+    df_tm = df_tm.rename(columns={"TEAM_ID": "HOME_TEAM_ID"})
     df = pd.merge(
         df,
-        df_tms[["HOME_TEAM_ID", "ARENACAPACITY", "YEARFOUNDED", "CONFERENCE"]],
+        df_tm[[homID, "ARENACAPACITY", "YEARFOUNDED", "CONFERENCE"]],
         on="HOME_TEAM_ID",
         how="left",
     )
+    df = df.rename(
+        columns={
+            "YEARFOUNDED": "YEARFOUNDED_homeTeam",
+            "CONFERENCE": "CONFERENCE_homeTeam",
+            "ARENACAPACITY": "ARENACAPACITY_homeTeam",
+        }
+    )
 
-    # Feature 4: Current win percentage for Home Team
-    df_rnk.sort_values("STANDINGSDATE", inplace=True)
-    df_rnk.set_index("STANDINGSDATE", inplace=True)
+    # Feature 4: Away Team arena capacity
+    # Feature 5: Year Away Team was founded
+    # Feature 6: Conference of Away Team, West=0 and East=1
+    visID = "VISITOR_TEAM_ID"
+    df_tm = df_tm.rename(columns={"HOME_TEAM_ID": "VISITOR_TEAM_ID"})
+    df = pd.merge(
+        df,
+        df_tm[[visID, "ARENACAPACITY", "YEARFOUNDED", "CONFERENCE"]],
+        on="VISITOR_TEAM_ID",
+        how="left",
+    )
+    df = df.rename(
+        columns={
+            "YEARFOUNDED": "YEARFOUNDED_awayTeam",
+            "CONFERENCE": "CONFERENCE_awayTeam",
+            "ARENACAPACITY": "ARENACAPACITY_awayTeam",
+        }
+    )
+
+    # Feature 7: Current win percentage for Home Team
+    df_rnk.drop(
+        [
+            "LEAGUE_ID",
+            "SEASON_ID",
+            "CONFERENCE",
+            "TEAM",
+            "G",
+            "W",
+            "L",
+            "HOME_RECORD",
+            "ROAD_RECORD",
+        ],
+        axis=1,
+        inplace=True,
+    )
     df = pd.merge_asof(
         df,
         df_rnk.add_suffix("_homeTeam"),
@@ -76,6 +120,23 @@ def main():
         right_by="TEAM_ID" + "_homeTeam",
         allow_exact_matches=False,
     )
+    df = df.dropna()
+    homStd = "STANDINGSDATE_homeTeam"
+    df.drop(["TEAM_ID_homeTeam", homStd], axis=1, inplace=True)
+
+    # Feature 8: Current win percentage for Home Team
+    df = pd.merge_asof(
+        df,
+        df_rnk.add_suffix("_awayTeam"),
+        left_index=True,
+        right_index=True,
+        left_by="VISITOR_TEAM_ID",
+        right_by="TEAM_ID" + "_awayTeam",
+        allow_exact_matches=False,
+    )
+    df = df.dropna()
+    visStd = "STANDINGSDATE_awayTeam"
+    df.drop(["TEAM_ID_awayTeam", visStd], axis=1, inplace=True)
 
     print(df)
 
